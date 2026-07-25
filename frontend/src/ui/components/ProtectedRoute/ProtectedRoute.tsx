d541200c9b0caecac7374b1f829da7071ae3d611
jhonenovate <166523215+jhonenovate@users.noreply.github.com>
2026-07-25 12:01:35 -0500

    fix

--- frontend/src/ui/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/ui/components/ProtectedRoute/ProtectedRoute.tsx
@@ -3,7 +3,6 @@ import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '../../../infrastructure/store/store';
 import type { UserRole } from '../../../domain/member/member.types';
 import { LoadingScreen } from '../LoadingScreen/LoadingScreen';
-import styles from './ProtectedRoute.module.css';
 
 interface ProtectedRouteProps {
   allowedRoles: UserRole[];
